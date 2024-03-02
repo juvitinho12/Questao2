c0e6b667a8cf322370a28f6bcd40f1596a5bb74d
João Vitor <vitor.oliveira@alu.ufc.br>
2024-03-02 17:40:56 -0300

    Questao2 v2
    Mudança das funções usadas na questão 2, mas ainda incompleta

--- Questao2.py
+++ Questao2.py
@@ -12,58 +12,68 @@ def pot_rec(pot_trans, dist, d_0):
 
     return pot_rec_result
 
-def distribuir_APs(M): #M é o número de APs
-    if M not in [1, 4, 9, 16, 25, 36, 49, 64]:
-        return None
-
-    tamanho_quadrado = 1000
-    lado_quadrado = int(np.sqrt(M))
-
-    tamanho_celula = tamanho_quadrado // lado_quadrado
-
-    # Criar coordenadas usando meshgrid
-    x, y = np.meshgrid(np.arange(0.5 * tamanho_celula, tamanho_quadrado, tamanho_celula),
-                      np.arange(0.5 * tamanho_celula, tamanho_quadrado, tamanho_celula))
-
-    coordenadas_APs = np.column_stack((x.ravel(), y.ravel()))
-
-    return coordenadas_APs
-
-def dAPUE(x_coord, y_coord, K, M): #K é o número de UEs e M é a coordenada da AP
-  dAPUE = []
+def dAPUE(x_coord, y_coord, K): #K é o número de UEs
+  dAPUE = np.array(K)
   for i in range(K):
-    dAPUE[i] = np.linalg.norm(np.array([x_coord[i], y_coord[i]]) - M)
+    dAPUE[i] = np.linalg.norm(np.array([x_coord[i], y_coord[i]]) - np.array([500,500]))
   return dAPUE
 
 
 def simular_experimento(B_t, p_t, d_0, K_0, M, N, K):
-    x_coord = []
-    y_coord = []
-    distanciaAPUE = []
-    coord_AP = distribuir_APs(M)
-    potencia_recebida = []
+    x_coord = np.zeros(K)
+    y_coord = np.zeros(K)
+    distanciaAPUE = np.zeros(K)
+    potencia_recebida = np.zeros(K)
     p_n = K_0 * (B_t / N)
     SNR = []
     SINR = []
-    capacidade = []
 
     for i in range(K):
         x_coord[i] = random.random() * 1000
         y_coord[i] = random.random() * 1000
-        distanciaAPUE[i] = dAPUE(x_coord[i], y_coord[i], K, coord_AP)
+        distanciaAPUE[i] = dAPUE(x_coord[i], y_coord[i], K)
         potencia_recebida[i] = pot_rec(p_t, distanciaAPUE[i], d_0)
-        if N <= K:
-            SNR[i] = potencia_recebida[i]/p_n
-        else:
+    
+
+
+    B_c = B_t / N
+    Capacidade = np.zeros(K)
+
+    #Penso em fazer a condicional de ser cálculo de SNR ou SINR no mesmo laço for, assim com isso eu posso colocar quando o valor for igual a 0 ser nulo, para que assim eu junte os vetores num só para melhor cálculo da SINR
+    SNR_SINR = np.concatenate((SNR, SINR))
+    for i in range(K):
+      Capacidade[i] = B_c * np.log2(1+SNR_SINR)
+
+    return Capacidade
+'''Cálculo da SINR em laço for:
             outras_estacoes = np.delete(potencia_recebida, i)
             interferencia = np.sum(outras_estacoes)
-            SINR[i] = potencia_recebida[i] / (interferencia + p_n)
-    B_c = B_t / N
-    snr_2 = np.sum(SNR)
-    Capacidade = []
-    if N <= K:
-      Capacidade = B_c * np.log2(1+snr_2)
-    else:
-       for i in range(K):
-          Capacidade[i] == B_c * np.log2(1+SINR)
-    return Capacidade
+            SINR[i] = potencia_recebida[i] / (interferencia + p_n)'''
+
+
+
+########################################################################################################################################
+B_t, p_t, d_0, K_0 = 100e6, 1e3, 1, 1e-17 # Em MHz, mW, metros, mW/Hz respectivamente
+M, K, N = 1, 10, 9 #Número de APs, UEs e Canais respectivamente
+
+# Número de iterações
+num_iteracoes = 1000
+
+# Armazenar todas as capacidades
+Capacidade_total = []
+
+# Iteração
+for i in range(num_iteracoes):
+    Capacidade_iteracao = simular_experimento(B_t, p_t, d_0, K_0, M, N, K)
+    Capacidade_total = np.concatenate((Capacidade_total, Capacidade_iteracao))
+
+#Deixando em ordem crescente
+x = np.sort(Capacidade_total)
+
+# Plotar apenas o eixo x em decibéis
+plt.xlabel('Capacidade')
+plt.ylabel('Porcentagem')
+plt.title('CDF da Capacidade')
+
+plt.plot(x, np.arange(0, len(Capacidade_total)) / len(Capacidade_total))
+plt.show()
